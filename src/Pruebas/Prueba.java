7e2d8735cd6b408ef984a5427d18dca021565680
“jother043” <josemgh166@gmail.com>
2022-11-18 00:09:40 +0100

    Add existing file

--- src/Pruebas/Prueba.java
+++ src/Pruebas/Prueba.java
@@ -2,6 +2,10 @@ package Pruebas;
 
 public class Prueba {
     public static void main(String[] args) {
-        
+        try {
+            int monedero;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
